13ba8d906b0e58eadc8c248cac58ed800fa0249a
Roman Cvelbar <91563406+cvelbar@users.noreply.github.com>
2021-11-16 19:25:23 +0000

    fix

--- student.py
+++ student.py
@@ -63,6 +63,7 @@ class Piggy(PiggyParent):
       
     def roman(self):
       while True:
+        self.fwd()
         if self.read_distance() < 200:
           self.right(primary =40, counter = -40)
           time.sleep(1)

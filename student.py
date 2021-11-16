c530274b602053fa7371d6ff01805c3c51bfc622
Roman Cvelbar <91563406+cvelbar@users.noreply.github.com>
2021-11-16 20:09:11 +0000

    change

--- student.py
+++ student.py
@@ -65,7 +65,7 @@ class Piggy(PiggyParent):
       while True:
         self.fwd()
         if self.read_distance() < 200:
-          self.right(primary =40, counter = -40)
+          self.right(primary =180, counter = -40)
           time.sleep(1)
 
 

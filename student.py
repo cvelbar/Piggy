1a88cc19abea906cc694d4efc4c2e09ecf372019
Roman Cvelbar <91563406+cvelbar@users.noreply.github.com>
2021-12-02 19:28:29 +0000

    fix?

--- student.py
+++ student.py
@@ -59,24 +59,37 @@ class Piggy(PiggyParent):
         
     def roman(self):
       while True:
-        self.fwd
-        time.sleep(1)
 
-        if self.read_distance() > 200:
+        if self.read_distance() < 200:
           self.stop()
-          self.servo(MIDPOINT + 300)
+          self.servo(self.MIDPOINT + 300)
+          time.sleep(1)
+          left = self.read_distance()
+          self.servo(self.MIDPOINT - 300)
           time.sleep(1)
           right = self.read_distance()
-          if left > right:
-          self.right(primary = 40, counter =-40)
-          time.sleep(1)
-          self.fwd()
-          time.sleep(2)
-          self.left(primary = 40, counter =-40)
-          time.sleep(1)
+          if left > right :
+            self.left(primary = 40, counter =-40)
+            time.sleep(1)
+            self.fwd()
+            time.sleep(2)
+            self.right(primary = 40, counter =-40)
+            time.sleep(1)
+            self.servo(self.MIDPOINT)
+            
 
+          elif right > left :
+            self.right(primary = 43, counter =-40)
+            time.sleep(1)
+            self.fwd()
+            time.sleep(2)
+            self.left(primary = 41, counter =-37)
+            time.sleep(1)
+            self.servo(self.MIDPOINT)
+        
         else:
           self.fwd()
+
 
     def roman2(self):
       #make square
